da88fe791476177b56bd8f30d66d30d945f3e2cc
mcerarslan <erarslanmehmetcan@gmail.com>
2024-07-04 14:59:34 +0300

    User View and Details

---
+++ RunGroopWebApp/Controllers/UserController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RunGroopWebApp.Interfaces;
+using RunGroopWebApp.ViewModels;
+using System.Net.WebSockets;
+
+namespace RunGroopWebApp.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UserController(IUserRepository userRepository) 
+        { 
+            _userRepository = userRepository;
+        }
+        [HttpGet("users")]
+        public async Task<IActionResult> Index()
+        {
+            var users = await _userRepository.GetAllUsers();
+            List<UserViewModel> result = new List<UserViewModel>();
+            foreach (var user in users)
+            {
+                var userVM = new UserViewModel()
+                {
+                    Id = user.Id,
+                    UserName = user.UserName,
+                    Pace = user.Pace,
+                    Mileage = user.Mileage,
+
+                };
+                result.Add(userVM);
+            }
+            return View(result);
+        }
+        public async Task<IActionResult> Detail(string id)
+        {
+            var user = await _userRepository.GetUserById(id);
+            var userDetailVM = new UserDetailViewModel()
+            {
+                Id = user.Id,
+                UserName = user.UserName,
+                Pace = user.Pace,
+                Mileage = user.Mileage,
+            };
+            return View(userDetailVM);
+        }
+    }
+}
---
+++ RunGroopWebApp/Interfaces/IUserRepository.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+using RunGroopWebApp.Models;
+
+namespace RunGroopWebApp.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<IEnumerable<AppUser>> GetAllUsers();
+        Task<AppUser> GetUserById(string id);
+        bool Add(AppUser user);
+        
+        bool Update(AppUser user);
+        bool Delete(AppUser user);
+        bool Save();
+
+    }
+}
